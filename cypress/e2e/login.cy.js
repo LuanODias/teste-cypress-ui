f7cd8caa30b44b865b9d2ccd2b4a1a06de12fe77
natskoe <koehlernathy@gmail.com>
2024-12-04 11:34:42 -0300

    Automação teste de login

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress"/>
+
+context('Funcionalidade Login', () =>{
+
+    it('Deve fazer login com sucesso', () =>{
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain', 'Minha conta')
+    })
+
+    it('deve exibir uma mensagem de erro ao inserir usuário', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Erro: O usuário aluno_ebac@teste não está registrado neste site.')
+    })
+
+    it('deve exibir uma mensagem de erro ao inserir uma senha inválida', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+    })
+})
