ec724c50e09dd2995c79adf92d345a61a4064e31
LuanODias <luoliveidias@gmail.com>
2025-01-26 18:10:22 -0300

    automação de login utilizando variáveis

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,22 +1,39 @@
 /// <reference types="cypress"/>
+const perfil = require("../fixtures/perfil.json")
 
 context('Funcionalidade Login', () =>{
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta/')
     });
 
     afterEach(() => {
         cy.screenshot()
     });
     
-
-    it('Deve fazer login com sucesso', () =>{
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.page-title').should('contain', 'Minha conta')
-    })
+    });
+
+    it('Deve fazer login com sucesso - Usando arquivo de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain', 'Minha conta')
+    });
+
+    it.only('Deve fazer login com sucesso - Usando fixtures', () => {
+        cy.fixture('perfil').then(dados =>{
+
+        cy.get('#username').type(dados.usuario)
+        cy.get('#password').type(dados.senha, {log: false})
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain', 'Minha conta')
+        });
+    });
 
     it('deve exibir uma mensagem de erro ao inserir usuário', () => {
         cy.get('#username').type('aluno_ebac@teste')
